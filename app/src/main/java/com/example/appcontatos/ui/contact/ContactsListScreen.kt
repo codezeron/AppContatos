043a414fac590f4e987b42ee44dbc0ba06d1db58
codezeron <enzohashimoto.2019@alunos.utfpr.edu.br>
2025-05-17 09:09:04 -0300

    feat: Criado componente ContactAvatar

--- app/src/main/java/com/example/appcontatos/ui/contact/ContactsListScreen.kt
+++ app/src/main/java/com/example/appcontatos/ui/contact/ContactsListScreen.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appcontatos.R
 import com.example.appcontatos.data.Contact
+import com.example.appcontatos.ui.contact.composables.ContactAvatar
 import com.example.appcontatos.ui.theme.AppContatosTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
@@ -318,7 +319,12 @@ fun ContactListItem(
         headlineContent = {
             Text(contact.fullName)
         },
-        leadingContent = {},
+        leadingContent = {
+            ContactAvatar(
+                firstName = contact.firstName,
+                lastName = contact.lastName,
+            )
+        },
         trailingContent = {
             IconButton(
                 onClick = {
